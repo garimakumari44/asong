6fbcf36d6899d24159857ac491a32e899a0b601a
Garima Kumari <garimakumari265@gmail.com>
2025-03-30 02:37:15 +0530

    Update 1

--- context/Provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { SessionProvider, SessionProviderProps, useSession } from "next-auth/react"
-
-export default function Provider({children, session}: SessionProviderProps) {
-    return <SessionProvider session={session}> 
-    {children}
-
-    </SessionProvider>
-}
