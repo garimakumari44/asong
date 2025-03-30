b9ccbdc6794e2cbe78600628ab3a5ef820ee3315
Garima Kumari <garimakumari265@gmail.com>
2025-03-30 22:26:54 +0530

    Fixing redirect 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,13 @@ import Footer from "./components/ui/footer";
 
 export default function Home() {
   return (
-    <div  className="w-full bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100 ">
+    <div  className="w-full bg-gradient-to-r from-indigo-100 via-purple-100 to-pink-100  ">
       
        
       <Navbar/>
-      
+      <div className="p-20"> 
       <Hero text="Violin's playing and the angels crying  When the Stars aligning I'll be there "/>
+      </div>
       <SectionOne/>
       <SectionTwo/>
       <Footer/>
